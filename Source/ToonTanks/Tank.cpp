20f0a61d22c109a992c5dc1281cf9469d8cf33f5
Aban Alfaify <faifyan@gmail.com>
2025-09-26 18:04:32 +0300

    5.11 creating tank child class

--- Source/ToonTanks/Tank.cpp
+++ Source/ToonTanks/Tank.cpp
@@ -2,4 +2,14 @@
 
 
 #include "Tank.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Camera/CameraComponent.h"
 
+ATank::ATank()
+{
+	SpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("Spring Arm"));
+	SpringArm->SetupAttachment(RootComponent);
+
+	Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
+	Camera->SetupAttachment(SpringArm);
+}
--- Source/ToonTanks/Tank.h
+++ Source/ToonTanks/Tank.h
@@ -14,4 +14,16 @@ class TOONTANKS_API ATank : public ABasePawn
 {
 	GENERATED_BODY()
 	
+
+public:
+
+	ATank();
+
+private:
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+	class USpringArmComponent* SpringArm;
+
+
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+	class UCameraComponent* Camera;
 };
